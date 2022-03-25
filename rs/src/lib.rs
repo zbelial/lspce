25af5eeea396d5a7a3a79ad0bdf39ebb04263d67
zbelial <zjytj@qq.com>
2022-03-25 19:25:18 +0800

    wip

--- rs/src/lib.rs
+++ rs/src/lib.rs
@@ -1,3 +1,19 @@
+#![allow(unused)]
 fn main() {
-    println!("Hello, world!");
+    use emacs::{defun, Env, Result, Value};
+
+    // Emacs won't load the module without this.
+    emacs::plugin_is_GPL_compatible!();
+
+    // Register the initialization hook that Emacs will call when it loads the module.
+    #[emacs::module]
+    fn init(env: &Env) -> Result<Value<'_>> {
+        env.message("Done loading!")
+    }
+
+    // Define a function callable by Lisp code.
+    #[defun]
+    fn say_hello(env: &Env, name: String) -> Result<Value<'_>> {
+        env.message(&format!("Hello, {}!", name))
+    }
 }
