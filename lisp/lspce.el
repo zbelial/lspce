a44f6bb79b4d722d863ed1fd44c06cb5cede4fe1
zbelial <zjytj@qq.com>
2022-04-27 23:05:11 +0800

    wip

--- lisp/lspce.el
+++ lisp/lspce.el
@@ -1,37 +1,197 @@
 ;;; lspce.el --- LSP client for Emacs -*- lexical-binding: t; -*-
 
 
-(add-to-list 'load-path "/mnt/Personal/VCS/bitbucket/lspce/rs/target/debug")
+;;; Require
+(require 'json)
+(require 'cl-lib)
+(require 'project)
+(require 'url-util)
+(require 'compile)                      ; for some faces
+(require 'warnings)
 
-(require 'lspce-module)
+;;; User tweakable stuff
+(defgroup lspce nil
+  "Interaction with Language Server Protocol servers"
+  :prefix "lspce-"
+  :group 'applications)
+
+(defface lspce-mode-line
+  '((t (:inherit font-lock-constant-face :weight bold)))
+  "Face for package-name in LSPCE's mode line.")
+
+
+(defcustom lspce-connect-timeout 30
+  "Number of seconds before timing out LSP connection attempts.
+If nil, never time out."
+  :type 'number)
+
+(defcustom lspce-autoshutdown nil
+  "If non-nil, shut down server after killing last managed buffer."
+  :type 'boolean)
+
+(defcustom lspce-send-changes-idle-time 0.5
+  "Don't tell server of changes before Emacs's been idle for this many seconds."
+  :type 'number)
+
+(defcustom lspce-doc-tooltip-font nil
+  "The font of documentation tooltip.
+
+Font format follow rule: fontname-fontsize."
+  :type 'string)
+
+(defcustom lspce-doc-tooltip-border-width 15
+  "The border width of lspce tooltip, default is 15 px."
+  :type 'integer)
+
+(defcustom lspce-doc-tooltip-timeout 30
+  "The timeout of lspce tooltip show time, in seconds."
+  :type 'integer)
+
+(defcustom lspce-doc-name "*lspce doc*"
+  "The name of lspce tooltip name."
+  :type 'string)
+
+(defcustom lspce-completion-ignore-case t
+  "If non-nil, ignore case when completing."
+  :type 'boolean)
+
+
+;;; Constants
+;;;
+(defconst lspce--symbol-kind-names
+  `((1 . "File") (2 . "Module")
+    (3 . "Namespace") (4 . "Package") (5 . "Class")
+    (6 . "Method") (7 . "Property") (8 . "Field")
+    (9 . "Constructor") (10 . "Enum") (11 . "Interface")
+    (12 . "Function") (13 . "Variable") (14 . "Constant")
+    (15 . "String") (16 . "Number") (17 . "Boolean")
+    (18 . "Array") (19 . "Object") (20 . "Key")
+    (21 . "Null") (22 . "EnumMember") (23 . "Struct")
+    (24 . "Event") (25 . "Operator") (26 . "TypeParameter")))
+
+(defconst lspce--kind-names
+  `((1 . "Text") (2 . "Method") (3 . "Function") (4 . "Constructor")
+    (5 . "Field") (6 . "Variable") (7 . "Class") (8 . "Interface")
+    (9 . "Module") (10 . "Property") (11 . "Unit") (12 . "Value")
+    (13 . "Enum") (14 . "Keyword") (15 . "Snippet") (16 . "Color")
+    (17 . "File") (18 . "Reference") (19 . "Folder") (20 . "EnumMember")
+    (21 . "Constant") (22 . "Struct") (23 . "Event") (24 . "Operator")
+    (25 . "TypeParameter")))
+
+
+(defconst lspce--{} (make-hash-table) "The empty JSON object.")
+
+
+(defun lspce-current-column () (- (point) (point-at-bol)))
+
+(defvar lspce-current-column-function #'lspce-current-column
+  "Function to calculate the current column.
+
+This is the inverse operation of
+`lspce-move-to-column-function' (which see).  It is a function of
+no arguments returning a column number.  For buffers managed by
+fully LSP-compliant servers, this should be set to
+`lspce-lsp-abiding-column', and `lspce-current-column' (the default)
+for all others.")
+
+(defun lspce-lsp-abiding-column ()
+  "Calculate current COLUMN as defined by the LSP spec."
+  (/ (- (length (encode-coding-region (line-beginning-position)
+                                      (point) 'utf-16 t))
+        2)
+     2))
+
+(defun lspce--pos-to-lsp-position (&optional pos)
+  "Convert point POS to LSP position."
+  (lspce--widening
+   (list :line (1- (line-number-at-pos pos t)) ; F!@&#$CKING OFF-BY-ONE
+         :character (progn (when pos (goto-char pos))
+                           (funcall lspce-current-column-function)))))
+
+(defvar lspce-move-to-column-function #'lspce-move-to-column
+  "Function to move to a column reported by the LSP server.
+
+According to the standard, LSP column/character offsets are based
+on a count of UTF-16 code units, not actual visual columns.  So
+when LSP says position 3 of a line containing just \"aXbc\",
+where X is a multi-byte character, it actually means `b', not
+`c'. However, many servers don't follow the spec this closely.
+
+For buffers managed by fully LSP-compliant servers, this should
+be set to `lspce-move-to-lsp-abiding-column', and
+`lspce-move-to-column' (the default) for all others.")
+
+(defun lspce-move-to-column (column)
+  "Move to COLUMN without closely following the LSP spec."
+  ;; We cannot use `move-to-column' here, because it moves to *visual*
+  ;; columns, which can be different from LSP columns in case of
+  ;; `whitespace-mode', `prettify-symbols-mode', etc.  (github#296,
+  ;; github#297)
+  (goto-char (min (+ (line-beginning-position) column)
+                  (line-end-position))))
+
+(defun lspce-move-to-lsp-abiding-column (column)
+  "Move to COLUMN abiding by the LSP spec."
+  (cl-loop
+   initially (move-to-column column)
+   with lbp = (line-beginning-position)
+   for diff = (- column
+                 (/ (- (length (encode-coding-region lbp (point) 'utf-16 t))
+                       2)
+                    2))
+   until (zerop diff)
+   do (forward-char (/ (if (> diff 0) (1+ diff) (1- diff)) 2))))
+
+(defun lspce--lsp-position-to-point (pos-plist &optional marker)
+  "Convert LSP position POS-PLIST to Emacs point.
+If optional MARKER, return a marker instead"
+  (save-excursion
+    (goto-char (point-min))
+    (forward-line (min most-positive-fixnum
+                       (plist-get pos-plist :line)))
+    (unless (eobp) ;; if line was excessive leave point at eob
+      (let ((tab-width 1)
+            (col (plist-get pos-plist :character)))
+        (unless (wholenump col)
+          (lspce--warn
+           "Caution: LSP server sent invalid character position %s. Using 0 instead."
+           col)
+          (setq col 0))
+        (funcall lspce-move-to-column-function col)))
+    (if marker (copy-marker (point-marker)) (point))))
 
 (defun lspce--path-to-uri (path)
   "URIfy PATH."
   (url-hexify-string
    (concat "file://" (if (eq system-type 'windows-nt) "/") (file-truename path))
    url-path-allowed-chars))
-(defvar lspce--jsonrpc-id 0)
 
+(defun lspce--uri-to-path (uri)
+  "Convert URI to a file path."
+  (when (keywordp uri) (setq uri (substring (symbol-name uri) 1)))
+  (let ((retval (url-filename (url-generic-parse-url (url-unhex-string uri)))))
+    (if (eq system-type 'windows-nt) (substring retval 1) retval)))
+
+(defvar lspce--jsonrpc-id 0)
 (defun lspce--jsonrpc-id ()
   (let ((id lspce--jsonrpc-id))
     (setq lspce--jsonrpc-id (1+ lspce--jsonrpc-id))
     id))
 
-(setq lspce--initialize-params (list :processId (emacs-pid)
-                                     :rootPath (expand-file-name default-directory)
-                                     :rootUri (lspce--path-to-uri default-directory)
-                                     :initializationOptions (make-hash-table)
-                                     :capabilities (make-hash-table)
-                                     ))
-(setq lspce--initialize-request (list 
-                                 :id (lspce--jsonrpc-id)
-                                 :method "initialize"
-                                 :params lspce--initialize-params))
+;;; Minor modes
+;;;
+(defvar lspce-mode-map (make-sparse-keymap))
 
-(lspce-module-server-running "/mnt/Personal/VCS/bitbucket/lspce" "rust")
-;; (lspce-module-connect "/mnt/Personal/VCS/bitbucket/lspce" "cat" "cat" "" (json-encode lspce--initialize-request))
-;; (lspce-module-connect "/mnt/Personal/VCS/bitbucket/lspce" "rust" "rust-analyzer" "proc-macro" (json-encode lspce--initialize-request))
-;; (lspce-module-connect "/mnt/Personal/VCS/bitbucket/lspce" "rust" "rust-analyzer" "--log-file /tmp/ra.log -v" (json-encode lspce--initialize-request))
-;; (lspce-module-connect "/mnt/Personal/VCS/bitbucket/lspce" "go" "gopls" "-logfile=/tmp/gopls.log" (json-encode lspce--initialize-request))
-;; (lspce-module-connect "/mnt/Personal/VCS/bitbucket/lspce" "go" "gopls" "" (json-encode lspce--initialize-request))
+(defvar lspce--managed-mode)              ; forward decl
 
+(defun lspce-managed-p ()
+  "Tell if current buffer is managed by LSPCE."
+  lspce--managed-mode)
+
+(defvar lspce-managed-mode-hook nil
+  "A hook run by LSPCE after it started/stopped managing a buffer.
+Use `lspce-managed-p' to determine if current buffer is managed.")
+
+
+
+(provide 'lspce)
---
+++ lisp/test.el
@@ -0,0 +1,37 @@
+;;; lspce.el --- LSP client for Emacs -*- lexical-binding: t; -*-
+
+
+(add-to-list 'load-path "/mnt/Personal/VCS/bitbucket/lspce/rs/target/debug")
+
+(require 'lspce-module)
+
+(defun lspce--path-to-uri (path)
+  "URIfy PATH."
+  (url-hexify-string
+   (concat "file://" (if (eq system-type 'windows-nt) "/") (file-truename path))
+   url-path-allowed-chars))
+(defvar lspce--jsonrpc-id 0)
+
+(defun lspce--jsonrpc-id ()
+  (let ((id lspce--jsonrpc-id))
+    (setq lspce--jsonrpc-id (1+ lspce--jsonrpc-id))
+    id))
+
+(setq lspce--initialize-params (list :processId (emacs-pid)
+                                     :rootPath (expand-file-name default-directory)
+                                     :rootUri (lspce--path-to-uri default-directory)
+                                     :initializationOptions (make-hash-table)
+                                     :capabilities (make-hash-table)
+                                     ))
+(setq lspce--initialize-request (list 
+                                 :id (lspce--jsonrpc-id)
+                                 :method "initialize"
+                                 :params lspce--initialize-params))
+
+(lspce-module-server-running "/mnt/Personal/VCS/bitbucket/lspce" "rust")
+;; (lspce-module-connect "/mnt/Personal/VCS/bitbucket/lspce" "cat" "cat" "" (json-encode lspce--initialize-request))
+;; (lspce-module-connect "/mnt/Personal/VCS/bitbucket/lspce" "rust" "rust-analyzer" "proc-macro" (json-encode lspce--initialize-request))
+;; (lspce-module-connect "/mnt/Personal/VCS/bitbucket/lspce" "rust" "rust-analyzer" "--log-file /tmp/ra.log -v" (json-encode lspce--initialize-request))
+;; (lspce-module-connect "/mnt/Personal/VCS/bitbucket/lspce" "go" "gopls" "-logfile=/tmp/gopls.log" (json-encode lspce--initialize-request))
+;; (lspce-module-connect "/mnt/Personal/VCS/bitbucket/lspce" "go" "gopls" "" (json-encode lspce--initialize-request))
+
