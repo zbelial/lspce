d9c2fa8c0218b9c24d2dd5562bd51b223a9ff89a
zbelial <zjytj@qq.com>
2022-10-05 14:07:34 +0800

    bugfix

--- lspce-calltree.el
+++ lspce-calltree.el
@@ -4,7 +4,7 @@
 
 (declare-function lspce--query-call-hierarchy "lspce")
 (declare-function lspce--uri-to-path "lspce-util")
-(declare-function lspce--lsp-position-to-point2 "lspce")
+(declare-function lspce--widening "lspce-util")
 
 (defcustom lspce-call-hierarchy-display-method 'ivy
   "How to display call hierarchy. Only support ivy ATM."
@@ -20,17 +20,18 @@
     (let (result
           label
           name kind detail uri range start end
-          start-pos info)
+          start-line start-column info)
       (setq name (gethash "name" item)
             uri (gethash "uri" item)
             range (gethash "range" item))
       (setq start (gethash "start" range))
-      (setq start-pos (lspce--lsp-position-to-point2 start))
-      (setq info (list :uri uri :start-pos start-pos))
+      (setq start-line (1+ (gethash "line" start)))
+      (setq start-column (gethash "character" start))
+      (setq info (list :uri uri :start-line start-line))
 
       (setq label (format "%30s:%s%s%s"
                           (propertize (truncate-string-to-width (f-filename (lspce--uri-to-path uri)) 30) 'face 'compilation-info)
-                          (propertize (format "%4d" (1+ (gethash "line" start))) 'face 'compilation-line-number)
+                          (propertize (format "%4d" start-line) 'face 'compilation-line-number)
                           (make-string (* indent 4) ?\s) name ))
       (cons label info)))
 
@@ -59,11 +60,17 @@
                         ("v" (lambda (item)
                                (let* ((info (cdr item))
                                       (uri (plist-get info :uri))
-                                      (start-pos (plist-get info :start-pos))
+                                      (start-line (plist-get info :start-line))
+                                      (start-column (plist-get info :start-column))
                                       (filename (lspce--uri-to-path uri))
-                                      (buffer (find-buffer-visiting filename)))
-                                 (find-file filename)
-                                 (goto-char start-pos)))
+                                      (buffer (find-file-noselect filename)))
+                                 (switch-to-buffer buffer)
+                                 (with-current-buffer buffer
+                                   (widen)
+                                   (goto-char (point-min))
+                                   (forward-line start-line)
+                                   (forward-char start-column)))
+                               )
                          ))))
 
   )
