e1a9247443181e14f911766cf4f46f7baf9accd8
zbelial <zjytj@qq.com>
2022-10-20 19:15:51 +0800

    display call tree in org format

--- lspce-calltree.el
+++ lspce-calltree.el
@@ -11,11 +11,18 @@
   :group 'lspce
   :type 'symbol)
 
-(when (featurep 'ivy)
-  (defvar lspce--incoming-call-indent 0)
-  (defvar lspce--incoming-call-items nil)
+(defvar lspce--incoming-call-indent 0)
+(defvar lspce--incoming-call-items nil)
 
-  (defun lspce--call-hierarchy-item-transfer (item indent)
+(defun lspce--call-hierarchy-item-collector (walker-fn)
+  (setq lspce--incoming-call-indent 0)
+  (setq lspce--incoming-call-items nil)
+
+  (when-let ((tree (lspce--query-incoming-calls)))
+    (funcall walker-fn tree)))
+
+(when (featurep 'ivy)
+  (defun lspce--call-hierarchy-ivy-item-transfer (item indent)
     "ivy transfer"
     (let (result
           label
@@ -35,26 +42,21 @@
                           (make-string (* indent 4) ?\s) name ))
       (cons label info)))
 
-  (defun lspce--call-hierarchy-walker (item-list)
+  (defun lspce--call-hierarchy-ivy-walker (item-list)
     (dolist (item item-list)
       (cond
        ((listp item)
         (setq lspce--incoming-call-indent (1+ lspce--incoming-call-indent))
-        (lspce--call-hierarchy-walker item)
+        (lspce--call-hierarchy-ivy-walker item)
         (setq lspce--incoming-call-indent (- lspce--incoming-call-indent 1)))
        (t
-        (cl-pushnew (lspce--call-hierarchy-item-transfer item lspce--incoming-call-indent) lspce--incoming-call-items)))))
+        (cl-pushnew (lspce--call-hierarchy-ivy-item-transfer item lspce--incoming-call-indent) lspce--incoming-call-items)))))
 
-  (defun lspce--call-hierarchy-item-collector ()
-    (let ((tree (lspce--query-incoming-calls)))
-      (setq lspce--incoming-call-indent 0)
-      (setq lspce--incoming-call-items nil)
-      (when tree
-        (lspce--call-hierarchy-walker tree))))
-
-  (defun lspce-incoming-calls ()
+  ;;;###autoload
+  (defun lspce-incoming-calls-to-ivy ()
+    "Fetch incoming calls to current symbol, render the result with ivy."
     (interactive)
-    (lspce--call-hierarchy-item-collector)
+    (lspce--call-hierarchy-item-collector #'lspce--call-hierarchy-ivy-walker)
     (ivy-read "调用链: " (reverse lspce--incoming-call-items)
               :action '(1
                         ("v" (lambda (item)
@@ -74,5 +76,50 @@
                          ))))
 
   )
+
+(defvar lspce--incoming-call-org-buffer "*Incoming Calls in Org*")
+(defun lspce--call-hierarchy-org-item-transfer (item indent)
+  "org transfer"
+  (let (label filename range start start-line)
+    (setq name (gethash "name" item))
+    (setq name (gethash "name" item)
+          filename (lspce--uri-to-path (gethash "uri" item))
+          range (gethash "range" item))
+    (setq start (gethash "start" range))
+    (setq start-line (1+ (gethash "line" start)))
+    
+    (setq label (format "%s %s\n%s" (make-string indent ?*) name
+                        (format "%s[[%s::%d][%s:%d]]" (make-string (1+ indent) ?\s) filename start-line (f-filename filename) start-line)))
+
+    label))
+
+(defun lspce--call-hierarchy-org-walker (item-list)
+  (dolist (item item-list)
+    (cond
+     ((listp item)
+      (setq lspce--incoming-call-indent (1+ lspce--incoming-call-indent))
+      (lspce--call-hierarchy-org-walker item)
+      (setq lspce--incoming-call-indent (- lspce--incoming-call-indent 1)))
+     (t
+      (cl-pushnew (lspce--call-hierarchy-org-item-transfer item lspce--incoming-call-indent) lspce--incoming-call-items)))))
+
+;;;###autoload
+(defun lspce-incoming-calls-to-org ()
+  "Fetch incoming calls to current symbol, render the result in orgmode format."
+  (interactive)
+  (let (call-items
+        buffer)
+    (lspce--call-hierarchy-item-collector #'lspce--call-hierarchy-org-walker)
+    (setq call-items (reverse lspce--incoming-call-items))
+    (if call-items
+        (progn
+          (setq buffer (get-buffer-create lspce--incoming-call-org-buffer))
+          (with-current-buffer buffer
+            (erase-buffer)
+            (dolist (ci call-items)
+              (insert ci "\n"))
+            (org-mode))
+          (switch-to-buffer buffer))
+      (message "No incoming calls."))))
 
 (provide 'lspce-calltree)
